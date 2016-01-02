28d7162b34a73798777a7fdbe678b6ec20cc6195
the-nexus <the-nexus@github.com>
2016-01-01 19:32:22 -0500

    Finished day 14 (with brute force step-by-step solution)

--- Days/day14.py
+++ Days/day14.py
@@ -1,7 +1,7 @@
 #open the input file
 lines = open("day14_input.txt", "r").readlines()
 
-duration = 1000 #seconds
+duration = 2503 #seconds
 
 #list the reindeers and their traits
 reindeers = []
@@ -13,6 +13,9 @@ for line in lines:
     reindeer["flytime"] = int(data[6])
     reindeer["resttime"] = int(data[13])
     reindeer["points"] = 0
+    reindeer["distance"] = 0
+    reindeer["moving"] = True
+    reindeer["timeLeft"] = reindeer["flytime"]
     reindeers.append(reindeer)
 
 
@@ -50,23 +53,53 @@ print("The leading reindeer is", lead[0], "at", lead[1], "m")
 #---------------------------------------------------------------------------------------
 #check which reindeer has the most points at 2503s
 
-def racePoints(racetime):
-    for time in range(1, racetime+1):
-        leader = raceDistance(time)
-        for reindeer in reindeers:
-            reindeerDist = distance(reindeer, time)
-            if reindeerDist == leader[1]:
-                reindeer["points"] = reindeer["points"] + 1
-    leadName = ""
-    leadPoints = 0
-    for reindeer in reindeers:
-        print(reindeer)
-        if reindeer["points"] > leadPoints:
-            leadPoints = reindeer["points"]
-            leadName = reindeer["name"]
-    return (leadName, leadPoints)
+#def racePoints(racetime):
+#    for time in range(1, racetime+1):
+#        leader = raceDistance(time)
+#        for reindeer in reindeers:
+#            reindeerDist = distance(reindeer, time)
+#            if reindeerDist == leader[1]:
+#                reindeer["points"] = reindeer["points"] + 1
+#    leadName = ""
+#    leadPoints = 0
+#    for reindeer in reindeers:
+#        print(reindeer)
+#        if reindeer["points"] > leadPoints:
+#            leadPoints = reindeer["points"]
+#            leadName = reindeer["name"]
+#    return (leadName, leadPoints)
+#
+#lead = racePoints(duration)
 
-lead = racePoints(duration)
+
+for second in range(1, duration + 1):
+    leadDist = 0
+
+    for reindeer in reindeers:
+        reindeer["timeLeft"] = reindeer["timeLeft"] - 1
+        if reindeer["moving"]:
+            reindeer["distance"] = reindeer["distance"] + reindeer["speed"]
+        if reindeer["timeLeft"] == 0:
+            if reindeer["moving"]:
+                reindeer["moving"] = False
+                reindeer["timeLeft"] = reindeer["resttime"]
+            else:
+                reindeer["moving"] = True
+                reindeer["timeLeft"] = reindeer["flytime"]
+        if reindeer["distance"] > leadDist:
+            leadDist = reindeer["distance"]
+
+    for reindeer in reindeers:
+        if reindeer["distance"] == leadDist:
+            reindeer["points"] = reindeer["points"] + 1
+
+leadName = ""
+leadPoints = 0
+for reindeer in reindeers:
+    print(reindeer["name"], "has", reindeer["points"])
+    if reindeer["points"] > leadPoints:
+        leadPoints = reindeer["points"]
+        leadName = reindeer["name"]
 
 #print the results
-print("The leading reindeer is", lead[0], "at", lead[1], "points")
+print("The leading reindeer is", leadName, "at", leadPoints, "points")
