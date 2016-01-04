99f63e844f68c58ea24585e6c5cfb232f90e2611
Kevin Pulgar-Turcotte <kpt11294@hotmail.com>
2016-01-04 08:42:02 -0500

    Reorganized the folders and files for readability

---
+++ day17/day17.py
@@ -0,0 +1,8 @@
+#function that counts the amount of combinations possible
+def countCombinations(sizes
+
+
+#get the lines from the input file
+lines = open("day17_input.txt", "r").readlines()
+
+containers = 
