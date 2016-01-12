ca7f70e575d469442e7a7a6db90a9e5eaaa5710a
the-nexus <the-nexus@github.com>
2016-01-12 13:36:47 -0500

    Completed day 18

--- day18/day18.py
+++ day18/day18.py
@@ -1,10 +1,11 @@
+#function that draws a table of lights like in the input file
 def lightsToString(lights):
     lightStr = []
     for lightLine in lights:
         for l in lightLine:
-            if l == 1:
+            if l == 1:  #light on
                 lightStr.append("#")
-            else:
+            else:       #light off
                 lightStr.append(".")
         lightStr.append("\n")
     return "".join(lightStr)
@@ -14,37 +15,44 @@ def lightsToString(lights):
 #open the input file and parse the data
 lines = open("day18_input.txt", "r").readlines()
 
-width = len(lines[0])
+width = len(lines[0].replace("\n", ""))
 height = len(lines)
-maxIt = 4
+maxIt = 100
 
+#build the initial light array
 lights = [[0 for j in range(0, width)] for i in range(0, height)]
-
 for i in range(0, len(lines)):
     line = lines[i].replace("\n", "")
     for j in range(0, len(line)):
-        lights[i][j] = (1 if line[j] == "#" else 0)
-
-print(lightsToString(lights))
+        if (i == 0 or i == height-1) and (j == 0 or j == width-1):
+            lights[i][j] = 1
+        else:
+            lights[i][j] = (1 if line[j] == "#" else 0)
+#print("INIT")
+#print(lightsToString(lights))
 for it in range(0, maxIt):
     nextLights = [[0 for j in range(0, width)] for i in range(0, height)]
     for i in range(0, height):
         for j in range(0, width):
             count = 0
-            for ii in range(-1, 2):
-                line = i + ii
-                for jj in range(-1, 2):
-                    column = j + jj
-                    if line >= 0 and line < height and column >= 0 and column < width and line != i and column != j:
-                        count = count + lights[line][column]
-            if lights[i][j] == 1 and (count == 2 or count == 3):
+            if (i == 0 or i == height-1) and (j == 0 or j == width-1):
+                #print("(", i, ",", j, ")")
                 nextLights[i][j] = 1
-            elif lights[i][j] == 0 and count == 3:
-                nextLights[i][j] == 1
-            if i == 0 and j == 2:
-                print(count)
+            else:
+                for ii in range(-1, 2):
+                    line = i + ii
+                    for jj in range(-1, 2):
+                        column = j + jj
+                        #if not out of bounds or not the same position as the light we're setting increment count if lit
+                        if line >= 0 and line < height and column >= 0 and column < width and not (line == i and column == j):
+                            count = count + lights[line][column]
+                #define the new light state according to the conditions
+                if lights[i][j] == 1 and (count == 2 or count == 3):
+                    nextLights[i][j] = 1
+                elif lights[i][j] == 0 and count == 3:
+                    nextLights[i][j] = 1
     lights = nextLights
-    print(lightsToString(lights))
+    #print(lightsToString(lights))
 lightCount = 0
 for lightLine in lights:
     lightCount = lightCount + lightLine.count(1)
