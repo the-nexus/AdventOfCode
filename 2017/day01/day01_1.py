5b0b529fc17c901568e27c04a92e80daaa8d8998
Kevin Pulgar-Turcotte <kpt11294@hotmail.com>
2017-12-09 17:16:03 -0500

    Finished day 01

---
+++ 2017/day01/day01_1.py
@@ -0,0 +1,16 @@
+filename = "./day01/day01_input.txt"
+
+input_file = open(filename)
+line = input_file.readline()
+
+n_characters = len(line)
+idx_offset = 1
+
+sum_result = 0
+for idx in range(n_characters):
+    current_value = int(line[idx])
+    compare_value = int(line[(idx + idx_offset) % n_characters])
+    if current_value is compare_value:
+        sum_result = sum_result + current_value
+
+print ("The sum of the captcha is:", sum_result)
