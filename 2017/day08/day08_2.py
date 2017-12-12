ec7d840e9120723a9bcb8aa3d2878500d83344a6
Kevin Pulgar-Turcotte <kpt11294@hotmail.com>
2017-12-11 19:47:00 -0500

    Completed day 8

---
+++ 2017/day08/day08_2.py
@@ -0,0 +1,53 @@
+import sys
+
+def isConditionMet(value_a, comparator, value_b):
+    if comparator == "!=":
+        return value_a != value_b
+
+    if comparator == "==":
+        return value_a == value_b
+
+    if comparator == ">":
+        return value_a > value_b
+
+    if comparator == ">=":
+        return value_a >= value_b
+
+    if comparator == "<":
+        return value_a < value_b
+
+    if comparator == "<=":
+        return value_a <= value_b
+
+    return False
+
+
+
+filename = "./day08/day08_input.txt"
+input_file = open(filename)
+lines = input_file.readlines()
+
+registers = {}
+highest_value = -sys.maxint
+
+for line in lines:
+    instructions = line.split()
+    reg_key = instructions[0]
+    should_increment = (instructions[1] == "inc")
+    diff_value = int(instructions[2])
+    comp_reg_key = instructions[4]
+    comparator = instructions[5]
+    comp_value = int(instructions[6])
+
+    if reg_key not in registers.keys():
+        registers[reg_key] = 0
+    if comp_reg_key not in registers.keys():
+        registers[comp_reg_key] = 0
+    
+    if isConditionMet(registers[comp_reg_key], comparator, comp_value):
+        registers[reg_key] = registers[reg_key] + (diff_value if should_increment else -diff_value)
+
+    current_highest_value = max(registers.values())
+    highest_value = max(highest_value, current_highest_value)
+
+print("The largest register value ever encountered is", highest_value)
